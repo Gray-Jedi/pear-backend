5133647b6dbe9baf74e02a68744840a4d7d9de28
Kevin Henry <kevinhenry@mac.com>
2021-11-10 21:11:11 -0800

    Post Demo - Most Components Work

--- api/urls/product_urls.py
+++ api/urls/product_urls.py
@@ -3,6 +3,14 @@ from api.views import product_views as views
 
 urlpatterns = [
     path('', views.getProducts, name="products"),
+    path('create/', views.createProduct, name="product-create"),
     path('<str:pk>/', views.getProduct, name="product"),
+    path('<str:pk>/reviews/', views.createProductReview, name="create-review"),
+    # path('top/', views.getTopProducts, name='top-products'),
+    path('upload/', views.uploadImage, name="image-upload"),
+    path('update/<str:pk>/', views.updateProduct, name="product-update"),
     path('delete/<str:pk>/', views.deleteProduct, name="product-delete"),
 ]
+    
+    
+
